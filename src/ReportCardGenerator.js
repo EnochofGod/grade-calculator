e06417a97131285e9a989a09f721ea6ef102ad20
ahemforGod <adepateenoch@gmail.com>
2025-10-07 10:49:46 +0100

    second commit

--- src/ReportCardGenerator.js
+++ src/ReportCardGenerator.js
@@ -1,6 +1,6 @@
 
 import React from 'react';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import GlobalScriptsAndStyles from './GlobalScriptsAndStyles';
 import App from './App';
 import { DataProvider } from './DataProvider';
@@ -8,11 +8,11 @@ import { DataProvider } from './DataProvider';
 const ReportCardGenerator = () => (
   <>
     <GlobalScriptsAndStyles />
-    <BrowserRouter>
+    <HashRouter>
       <DataProvider>
         <App />
       </DataProvider>
-    </BrowserRouter>
+    </HashRouter>
   </>
 );
 
